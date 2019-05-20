2312842a9a235d536080d283ca216cec05339c2f
unknown <jamie313@gmail.com>
2019-05-21 11:57:20 +1200

    Tut1

--- Vehicle.UnitTests/car.cs
+++ Vehicle.UnitTests/car.cs
@@ -21,16 +21,34 @@ namespace Vehicle.UnitTests
         public void car_can_stop()
         {
             // TODO: arrange
+            var subject = new Car();
             // TODO: act
+            subject.Start();
+            subject.Stop();
             // TODO: assert
+            Assert.False(subject.Started, "Car hasn't stopped");
         }
 
         [Fact]
         public void car_cannot_move_forward_if_not_started()
         {
             // TODO: arrange
+            var subject = new Car();
             // TODO: act
+            
             // TODO: assert
+            Assert.Throws<Vehicle.CarNotStartedException>(()=>subject.MoveForward());
+        }
+
+        [Fact]
+        public void car_has_already_started()
+        {
+            // TODO: arrange
+            var subject = new Car();
+            // TODO: act
+            subject.Start();
+            // TODO: assert
+            Assert.Throws<Vehicle.CarAlreadyStartedException>(()=>subject.Start());
         }
 
         [Theory]
